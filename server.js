4e9f8066fe8186d20e40b23b520c61ad66018e25
Sona Shrestha <sona.shrestha004@gmail.com>
2021-07-01 18:23:07 +1000

    updating server js

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require("express");
-const mongojs = require("mongojs");
-const logger = require("morgan");
+const mongoose = require("mongoose");
 
-const databaseUrl = "warmup";
-const collections = ["books"];
-const db = mongojs(databaseUrl, collections);
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
-db.on("error", (error) => {
-  console.log("Database Error:", error);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+// routes
+// app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
