c2b5c41308ef8c531fd69d2be842aeb1e3da4b65
Sona Shrestha <sona.shrestha004@gmail.com>
2021-07-03 19:35:23 +1000

    adding get workout routes

--- routes/api.js
+++ routes/api.js
@@ -1,35 +1,17 @@
 const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 
-router.post("/api/workouts", ({ body }, res) => {
-  Workout.create(body)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
+const db = require("../models");
+
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .sort({ date: -1 })
+    .then((workout) => {
+      res.status(200).json(workout);
     })
     .catch((err) => {
       res.status(400).json(err);
     });
 });
-
-router.post("/api/workouts/bulk", ({ body }, res) => {
-  Workout.insertMany(body)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-// router.get("/api/transaction", (req, res) => {
-//     Workout.find({})
-//     .sort({ date: -1 })
-//     .then((dbTransaction) => {
-//       res.json(dbTransaction);
-//     })
-//     .catch((err) => {
-//       res.status(400).json(err);
-//     });
-// });
 
 module.exports = router;
