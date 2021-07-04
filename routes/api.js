7dce64123c7923ce3f436a19093fe1c0dd8f91da
Sona Shrestha <sona.shrestha004@gmail.com>
2021-07-04 13:56:01 +1000

    adding workout

--- routes/api.js
+++ routes/api.js
@@ -25,4 +25,28 @@ router.get("/api/workouts/range", (req, res) => {
     });
 });
 
+router.post("/api/workouts", (req, res) => {
+  console.log("here");
+  db.Workout.create(req.body)
+    .then((workout) => {
+      res.status(201).json(workout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", function ({ body, params }, res) {
+  db.Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 module.exports = router;
